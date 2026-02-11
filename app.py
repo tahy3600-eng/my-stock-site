0faefcb6ff5a17f1c4f1b792fbcfb8a7e24506e0
tahy3600-eng <tahy3600@gmail.com>
2026-02-11 13:15:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -128,5 +128,6 @@ def update_dashboard():
 # 실행
 update_dashboard()
 
-st.divider(김채원 사랑해)
-st.caption("※ 데이터 출처: Yahoo Finance (환율은 실시간 기준 15~20분 지연될 수 있습니다.)")
+st.divider()
+st.caption("※ 데이터 출처: Yahoo Finance (환율은 실시간 기준 15~20분 지연될 수 있습니다. 김채원 사랑해)")
+
